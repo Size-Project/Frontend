5a4bb25a79cacfacb381ea8806a9bfc126253fc5
lotus9630 <68753108+lotus9630@users.noreply.github.com>
2021-11-19 18:36:31 +0900

    page 작업중

---
+++ src/pages/Login.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+const PageSign = () => {
+  return <StyledWrapper>로그인 페이지</StyledWrapper>;
+};
+
+export default PageSign;
+
+const StyledWrapper = styled.div``;
