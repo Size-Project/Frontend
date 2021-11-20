54fb59e63a36bbebe5d59add5146f44981d5e64b
김진아 <wlsdk7245@gmail.com>
2021-11-20 14:16:13 +0900

    feat(layout): 상품 카드 수정

--- src/app.component/GoodsCard/GoodsCard.tsx
+++ src/app.component/GoodsCard/GoodsCard.tsx
@@ -20,8 +20,23 @@ const GoodsCard = () => {
           <span className="price-amount">{(269000).toLocaleString()}</span>
         </div>
         <div className="goods-review">
-          <span className="review-grade">4.5</span>
-          <span className="review-detail">리뷰 682</span>
+          <div className="review-star">
+            <svg
+              className="star-icon"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              preserveAspectRatio="xMidYMid meet"
+            >
+              <path
+                fill="currentColor"
+                fill-rule="evenodd"
+                d="M12 19.72l-5.677 2.405c-.76.322-1.318-.094-1.247-.906l.533-6.142-4.042-4.656c-.54-.624-.317-1.283.477-1.467l6.006-1.39L11.23 2.28c.426-.707 1.122-.699 1.542 0l3.179 5.282 6.006 1.391c.805.187 1.011.851.477 1.467l-4.042 4.656.533 6.142c.072.822-.497 1.224-1.247.906L12 19.72z"
+              ></path>
+            </svg>
+          </div>
+          <div className="review-grade">4.5</div>
+          <div className="review-detail">리뷰 682</div>
         </div>
         <div className="goods-tag"></div>
       </div>
@@ -81,9 +96,9 @@ const StyledWrapper = styled.div`
 
     .goods-price {
       font-size: 17px;
-      line-height: 23px;
       font-weight: 700;
       margin-bottom: 3px;
+      line-height: 23px;
       .price-off {
         color: #35c5f0;
         margin-right: 2px;
@@ -96,6 +111,18 @@ const StyledWrapper = styled.div`
     .goods-review {
       font-weight: 700;
       font-size: 12px;
+      display: flex;
+      align-items: center;
+
+      .review-star {
+        margin-right: 2px;
+
+        .star-icon {
+          height: 12px;
+          width: 12px;
+          color: #35c5f0;
+        }
+      }
 
       .review-grade {
         margin-right: 2px;
